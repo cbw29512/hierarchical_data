676ccd2d840eb8490294c37e24f44ccef260c374
cbw29512 <divclass01@gmail.com>
2020-06-11 16:13:08 -0400

    add readme

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
 from project.models import Tree
 
-admin.site.register(Tree, MPTTModelAdmin)
+admin.site.register(Tree, DraggableMPTTAdmin)
